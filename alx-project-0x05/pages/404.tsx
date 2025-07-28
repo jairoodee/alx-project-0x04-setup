2a4b78befda118e29a1d190d5e5bf4401cd63934
Odee Jairo <odee.jairo@gmail.com>
2025-07-28 15:52:27 +0300

    1. Duplicated the project alx-project-0x04 with a new name of alx-project-0x05.
    2. Created a new directory context/ in the project root directory
    
    3. Created an empty file CountContext.tsx under the context/ directory.
    
    4. Replaced the content of the CountContext.tsx.
    
    5. Modified _app.tsx
    
    6. Modified components/layouts/Header.tsx

---
+++ alx-project-0x05/pages/404.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+    
+
+const Custom404 = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex flex-col justify-center items-center text-white">
+      {/* Main Heading */}
+      <h1 className="text-6xl font-bold mb-4">Oops! 😱</h1>
+      <p className="text-2xl mb-8">
+        We can't seem to find the page you're looking for.
+      </p>
+
+      {/* Funny Message */}
+      <p className="text-lg mb-8 text-center max-w-md">
+        Maybe it was abducted by aliens 👽, or it just took a wrong turn into the Internet wilderness! Either way, it's not here.
+      </p>
+
+      {/* Button to navigate back */}
+      <Link
+        href="/"
+        className="flex items-center space-x-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg transition duration-300"
+      >
+        <FontAwesomeIcon icon={faHome} size="lg" />
+        <span>Go Back Home</span>
+      </Link>
+    </div>
+  );
+}
+
+export default Custom404;
